915e7d68bae4dc7a96a253edb10a4fe2f06d5d33
student04 <smeesathit@gmail.com>
2017-06-14 14:59:40 +0700

    Initial Commit

---
+++ Navigator Toolbar Multiple Screen/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Navigator Toolbar Multiple Screen
+//
+//  Created by Student04 on 6/14/2560 BE.
+//  Copyright © 2560 SNRU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
