ccbafca2f9ac9441c64d0c7ec27209f9c2aa9f0e
student04 <smeesathit@gmail.com>
2017-06-14 16:07:07 +0700

    Increase Decrease Reset success

--- Navigator Toolbar Multiple Screen/ViewController.swift
+++ Navigator Toolbar Multiple Screen/ViewController.swift
@@ -10,27 +10,50 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    var intNumber = 1
+    
+    
     // Handle increaseNumber button
     @IBAction func increaseNumber(_ sender: Any) {
         showMessage(strMessage: "increaseNumber clicked !")
         
+        intNumber += 1
+        showNumber(intNumber: intNumber)
+        
+        // numberLabel.text = String(intNumber)
     }
     
     // Handle decreaseNumber button
     @IBAction func decreaseNumber(_ sender: Any) {
         showMessage(strMessage: "decreaseNumber clicked !")
+
+        intNumber -= 1
+        showNumber(intNumber: intNumber)
+
+        // numberLabel.text = String(intNumber)
     }
     
     // Handle resetNumber button
     @IBAction func resetNumber(_ sender: Any) {
         showMessage(strMessage: "resetNumber clicked!")
+        
+        intNumber = 1
+        showNumber(intNumber: intNumber)
+
+        // numberLabel.text = String(intNumber)
     }
+    
+    @IBOutlet weak var numberLabel: UILabel!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     } // End Main method
     
+    func showNumber(intNumber: Int) -> Void {
+        let strNumber = String(intNumber)
+        numberLabel.text = strNumber
+    }
     // Show message on console
     func showMessage(strMessage: String) -> Void {
         print("Message ==> \(strMessage)")
