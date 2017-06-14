faf50bbe6f6aa6a7826c3f32c76ff65ed14fe9d7
student04 <smeesathit@gmail.com>
2017-06-14 15:51:35 +0700

    Basic navigation and toolbar button event

--- Navigator Toolbar Multiple Screen/ViewController.swift
+++ Navigator Toolbar Multiple Screen/ViewController.swift
@@ -9,19 +9,38 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    // Handle increaseNumber button
+    @IBAction func increaseNumber(_ sender: Any) {
+        showMessage(strMessage: "increaseNumber clicked !")
+        
+    }
+    
+    // Handle decreaseNumber button
+    @IBAction func decreaseNumber(_ sender: Any) {
+        showMessage(strMessage: "decreaseNumber clicked !")
+    }
+    
+    // Handle resetNumber button
+    @IBAction func resetNumber(_ sender: Any) {
+        showMessage(strMessage: "resetNumber clicked!")
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+    } // End Main method
+    
+    // Show message on console
+    func showMessage(strMessage: String) -> Void {
+        print("Message ==> \(strMessage)")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-    
     
 
-}
+} // End class
 
